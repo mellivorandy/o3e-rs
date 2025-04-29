9c4a9c4453c30124cc71b39b533313ae0da67a82
mellivorandy <anydoyi@gmail.com>
2025-04-29 08:26:23 +0800

    Update parser.rs

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -91,7 +91,7 @@ mod tests {
 
     #[test]
     fn parse_test() {
-        let content = fs::read_to_string("data/simple2.txt")
+        let content = fs::read_to_string("data/simple_0.txt")
             .expect("Failed to read txt file.");
 
         let metas = Parser::parse_file(&content);
